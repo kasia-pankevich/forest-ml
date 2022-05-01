d7ca569e2370df5358eaccb884aa4672a87a4af3
kasia-pankevich <86152422+kasia-pankevich@users.noreply.github.com>
2022-05-01 18:35:38 +0400

    Add model saving

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -1,14 +1,19 @@
 import click
+from pathlib import Path
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from joblib import dump
 
 @click.command()
-@click.option("-p", "--ds-path", default="data/train.csv")
-@click.option("-n", "--n-neighbors", default=5)
-@click.option("-r", "--rand-state", default=42)
-def train(ds_path: str, n_neighbors: int, rand_state) -> None:
+@click.option("-p", "--ds-path", default="data/train.csv", 
+                type=click.Path(exists=True, dir_okay=False, path_type=Path))
+@click.option("-n", "--n-neighbors", default=5, type=int)
+@click.option("-r", "--rand-state", default=42, type=int)
+@click.option("-s", "--save-model-path", default="data/model.joblib",
+                type=click.Path(dir_okay=False, writable=True, path_type=Path))
+def train(ds_path: Path, n_neighbors: int, rand_state: int, save_model_path: Path) -> None:
     data = pd.read_csv(ds_path)
     click.echo(f"Dataset shape: {data.shape}")
 
@@ -19,3 +24,4 @@ def train(ds_path: str, n_neighbors: int, rand_state) -> None:
     knn.fit(X_train, y_train)
     accuracy = accuracy_score(y_test, knn.predict(X_test))
     click.echo(f"Accuracy: {accuracy}")
+    dump(knn, save_model_path)
