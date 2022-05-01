a274b31814bc136c2d23af038817ee2338dbedb0
kasia-pankevich <86152422+kasia-pankevich@users.noreply.github.com>
2022-05-01 21:14:51 +0400

    Add mlflow logging

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -1,10 +1,13 @@
 import click
 from pathlib import Path
+import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_validate, StratifiedKFold
 from sklearn.metrics import make_scorer, matthews_corrcoef, f1_score
 from joblib import dump
+import mlflow
+import mlflow.sklearn
 
 @click.command()
 @click.option("-p", "--ds-path", default="data/train.csv", 
@@ -23,12 +26,21 @@ def train(ds_path: Path, n_neighbors: int, k_fold:int, shuffle: bool, rand_state
     X, y = data.drop(columns=["Id", "Cover_Type"]), data["Cover_Type"]
 
     knn = KNeighborsClassifier(n_neighbors)
-    kfold = StratifiedKFold(n_splits=k_fold, shuffle=shuffle, random_state=rand_state)
-    cv_results = cross_validate(knn, X, y, 
-                                scoring={"Matthews correlation": make_scorer(matthews_corrcoef), 
-                                         "Accuracy": "accuracy",
-                                         "F1": make_scorer(f1_score, average="weighted")}, 
-                                cv=kfold, return_estimator=True)
-
-    click.echo(f"Cross validation results: {cv_results}")
-    dump(knn, save_model_path)
+    with mlflow.start_run(run_name="KNN StratifiedKFold"):
+        kfold = StratifiedKFold(n_splits=k_fold, shuffle=shuffle, random_state=rand_state)
+        cv_results = cross_validate(knn, X, y, 
+                                    scoring={"Matthews correlation": make_scorer(matthews_corrcoef), 
+                                            "Accuracy": "accuracy",
+                                            "F1": make_scorer(f1_score, average="weighted")}, 
+                                    cv=kfold, return_estimator=True)
+        mlflow.sklearn.log_model(knn, "forest_knn")
+        mlflow.log_param("n_neighbors", n_neighbors)
+        mlflow.log_param("k_fold", k_fold)
+        mlflow.log_param("shuffle", shuffle)
+        mlflow.log_metric("matthews_corrcoef", np.mean(cv_results["test_Matthews correlation"]))
+        mlflow.log_metric("Accuracy score", np.mean(cv_results["test_Accuracy"]))
+        mlflow.log_metric("F1 score", np.mean(cv_results["test_F1"]))
+        
+        click.echo(f"Cross validation results: {cv_results}")
+        dump(knn, save_model_path)
+        click.echo(f"Model has been saved to {save_model_path}")
