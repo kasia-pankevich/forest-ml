133f995b30c99a0a86438d89972056c3298570b4
kasia-pankevich <86152422+kasia-pankevich@users.noreply.github.com>
2022-05-03 17:20:04 +0400

    Fix parameter

--- src/forest_ml/eda.py
+++ src/forest_ml/eda.py
@@ -6,9 +6,9 @@ from pandas_profiling import ProfileReport
 @click.option("-d", "--ds-path", default="data/train.csv",
                 help="Path to train dataset csv")
 @click.option("-s", "--save-output-path", default="forest_report.html",
-                help="Local folder path and file name to save model to")
-def profile(ds_path: str, output_filename: str) -> None:
+                help="Local folder path and file name to save EDA report to")
+def profile(ds_path: str, save_output_path: str) -> None:
     data = pd.read_csv(ds_path)
     profile = ProfileReport(data, title="Forest Cover Type Profiling Report", explorative=True)
-    profile.to_file(output_filename)
-    click.echo(f"View report in file '{output_filename}'")
+    profile.to_file(save_output_path)
+    click.echo(f"View report in file '{save_output_path}'")
