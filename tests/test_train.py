e6db83bba8e756cf9254c1c148fb85c16a809fff
kasia-pankevich <86152422+kasia-pankevich@users.noreply.github.com>
2022-05-05 16:38:38 +0400

    Add test for valid usage with isolated file system

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,7 +1,12 @@
 from click.testing import CliRunner
+import click
 import pytest
+from joblib import load
+import pandas as pd
+from pathlib import Path
 
 from forest_ml.train import train
+from forest_ml.predict import predict
 import forest_ml.eda
 
 @pytest.fixture
@@ -14,6 +19,30 @@ def test_error_for_invalid_preproc(runner: CliRunner) -> None:
     assert "Invalid value for '--preproc'" in result.output
 
 def test_error_for_invalid_n_features(runner: CliRunner) -> None:
-    result = runner.invoke(train, ["--n-features", "100"])
+    result = runner.invoke(train, ["--n-features", 100])
     assert result.exit_code == 1
-    # assert "value" in result.output
+
+def test_for_valid_case(runner: CliRunner, tmp_path) -> None:
+    samle_path = Path("tests/sample.csv").resolve()
+    samle_test_path = Path("tests/sample_test.csv").resolve()
+    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+        mdl_path = "mdl.joblib"
+        result = runner.invoke(train, [
+                                        "-d", samle_path, 
+                                        "-s", mdl_path, 
+                                        "--nested-cv", False, 
+                                        "-m", "knn", 
+                                        "--n-neighbors", 1
+                                    ])
+        if result.exit_code != 0:
+            click.echo(result.output)
+        assert result.exit_code == 0
+        subm_path = tmp_path / "submissions.csv"
+        result = runner.invoke(predict, [
+                                        "-m", mdl_path, 
+                                        "-t", samle_test_path, 
+                                        "-s", subm_path
+                                    ])
+        y_predict = pd.read_csv(subm_path)
+        assert result.exit_code == 0
+        assert y_predict["Cover_Type"].isin(range(1, 8)).sum() == y_predict.shape[0]
